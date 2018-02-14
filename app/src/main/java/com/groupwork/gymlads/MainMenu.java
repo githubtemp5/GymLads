0a21e2c0ac68b5f12ae9e2f1f7c7c84f3ec1f8a5
Alvin Shrestha <UP826133@uni.ds.port.ac.uk>
2018-02-14 16:26:35 +0000

    added onStop onResume log methods
    logging implemented

--- app/src/main/java/com/groupwork/gymlads/MainMenu.java
+++ app/src/main/java/com/groupwork/gymlads/MainMenu.java
@@ -5,27 +5,61 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {  //extends Activity class
 
+    private static final String TAG ="gymladMessage"    //log tag, logs the current state of the activity/app.
+    //FIrst calls the method called onCreate when you begin the app
+    //might also have onDestroy method so when you close the app, it saves the current data, uploads to the database and so on
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.i(MainMenu.TAG, "onCreate");
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(MainMenu.TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(MainMenu.TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(MainMenu.TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(MainMenu.TAG, "onStop");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(MainMenu.TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(MainMenu.TAG, "onRestoreInstanceState");
     }
 
     @Override
