4f06f5be81e306f942d20c811ca1be7ee0b92b38
Alvin Shrestha <UP826133@uni.ds.port.ac.uk>
2018-02-20 11:13:35 +0000

    Properly redid the GUI for the profile.
    Redone the GUI with GridLayout, proper naming convention and id
    assigned.

--- app/src/main/java/com/groupwork/gymlads/MainMenu.java
+++ app/src/main/java/com/groupwork/gymlads/MainMenu.java
@@ -21,8 +21,8 @@ public class MainMenu extends AppCompatActivity {  //extends Activity class
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         Log.i(MainMenu.TAG, "onCreate");
     }
 
@@ -62,25 +62,4 @@ public class MainMenu extends AppCompatActivity {  //extends Activity class
         Log.i(MainMenu.TAG, "onRestoreInstanceState");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
