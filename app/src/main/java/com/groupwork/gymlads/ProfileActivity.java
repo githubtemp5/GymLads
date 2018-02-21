75481523ece86ca5a7abc4d4d10eedc6ea6b4acd
Alvin Shrestha <UP826133@uni.ds.port.ac.uk>
2018-02-21 12:32:13 +0000

    Minor frontend changes and building on backend
    Further expansion of retreiveAndValidateDetails(renamed from
    validateDetails) and changed EditTextBox for Gender to a dropdown
    Spinner. Added a new class User.

--- app/src/main/java/com/groupwork/gymlads/ProfileActivity.java
+++ app/src/main/java/com/groupwork/gymlads/ProfileActivity.java
@@ -1,25 +1,20 @@
 package com.groupwork.gymlads;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.sql.SQLOutput;
+import android.widget.Spinner;
 
 public class ProfileActivity extends AppCompatActivity {  //extends Activity class
 
     private static final String TAG ="gymladMessage";    //log tag, logs the current
-    private EditText fullNameTextBox, emailTextBox, ageTextBox, genderTextBox, weightTextBox, heightTextBox;
+    private EditText fullNameTextBox, emailTextBox, ageTextBox, weightTextBox, heightTextBox;
+    private Spinner genderDropdown;
+    private Button updButton;
     // state of the activity/app.
     //First calls the method called onCreate when you begin the app
     //might also have onDestroy method so when you close the app, it saves the current data, uploads to the database and so on
@@ -34,26 +29,32 @@ public class ProfileActivity extends AppCompatActivity {  //extends Activity cla
 
         //Intialising text boxes
 
-        fullNameTextBox = (EditText) findViewById(R.id.fullNameTextBox);
+        fullNameTextBox = findViewById(R.id.fullNameTextBox);
 
-        emailTextBox = (EditText) findViewById(R.id.emailTextBox);
+        emailTextBox = findViewById(R.id.emailTextBox);
 
-        ageTextBox = (EditText) findViewById(R.id.ageTextBox);
+        ageTextBox = findViewById(R.id.ageTextBox);
 
-        genderTextBox = (EditText) findViewById(R.id.genderTextBox);
+        genderDropdown = findViewById(R.id.genderList);
 
-        weightTextBox = (EditText) findViewById(R.id.weightTextBox);
+        weightTextBox = findViewById(R.id.weightTextBox);
 
-        heightTextBox = (EditText) findViewById(R.id.heightTextBox);
+        heightTextBox = findViewById(R.id.heightTextBox);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderDropdown.setAdapter(adapter);
+
 
 
         //Update Details Button
-        Button updButton = (Button) findViewById(R.id.updateButton);
+        updButton = findViewById(R.id.updateButton);
 
         updButton.setOnClickListener(
             new Button.OnClickListener(){
                 public void onClick(View v){
-        validateDetails();
+                    retrieveAndValidateDetails();
 
 
                 }
@@ -62,27 +63,72 @@ public class ProfileActivity extends AppCompatActivity {  //extends Activity cla
 
     }
 
-    public void validateDetails(){
-        String fullName = fullNameTextBox.getText().toString();
-        String email = emailTextBox.getText().toString();
-        int age = Integer.parseInt(ageTextBox.getText().toString());
-        String gender = genderTextBox.getText().toString();
-       // int weight = Integer.parseInt(weightTextBox.getText().toString());
+    public boolean retrieveAndValidateDetails(){
+        boolean fullNameCheck=false;
+        boolean emailCheck=false;
+        boolean ageCheck=false;
+        boolean weightCheck=false;
+        boolean heightCheck = false;
+        String fullName, email, ageString, weightString, heightString;
+
+        int age=0;
+        int weight=0;
+        double height = 0;
+
+        fullName = fullNameTextBox.getText().toString();
+        email = emailTextBox.getText().toString();
+
+        ageString = ageTextBox.getText().toString();
+        if(!ageString.equals("")){
+            age = Integer.parseInt(ageString);
+        }
+
+        String gender = genderDropdown.getSelectedItem().toString();
+
+        weightString = weightTextBox.getText().toString();
+        if(!weightString.equals("")){
+            weight = Integer.parseInt(weightString);
+        }
+
+        heightString = heightTextBox.getText().toString();
+        if(!heightString.equals("")){
+            height = Double.parseDouble(heightString);
+        }
+
+
        // double height = Double.parseDouble(heightTextBox.getText().toString());
 
         if(fullName.matches("[a-zA-Z]+ ([a-zA-Z]+ )*[a-zA-Z]+")){
             System.out.println("VALID NAME");
+            fullNameCheck = true;
         }
 
         if(email.matches("[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*@[a-zA-Z].[a-zA-Z]+(.[a-zA-Z]+)*")){
             System.out.println("VALID EMAIL");
+            emailCheck = true;
         }
 
         if(age>=15 && age<=150){
             System.out.println("VALID AGE");
+            ageCheck = true;
+        }
+        System.out.println(gender);
+
+        if(weight>=35 && weight<=1000){
+            System.out.println("VALID WEIGHT");
+            weightCheck = true;
+
+        }
+        if(height >= 54 && height<=260){
+            System.out.println("VALID HEIGHT");
+            heightCheck = true;
         }
 
-        
+
+        System.out.println(fullNameCheck && emailCheck && ageCheck && weightCheck && heightCheck);
+        return fullNameCheck && emailCheck && ageCheck && weightCheck && heightCheck;
+
+
 
 
 
---
+++ app/src/main/java/com/groupwork/gymlads/User.java
@@ -0,0 +1,19 @@
+package com.groupwork.gymlads;
+
+/**
+ * Created by up826133 on 21/02/2018.
+ */
+
+
+public class User() {
+private String userName ;
+
+    public User(String userName, String fullName, String gender){
+
+    }
+
+    public User(String userName, String fullName, String email, int age , String gender, int weight, double height){
+
+
+        }
+}
