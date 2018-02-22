b230e12c25499290418a66e2685e16b251a091fb
githubtemp5 <UP826133@uni.ds.port.ac.uk>
2018-02-22 23:18:02 +0000

    Trying to establish a connection to mysql server
    Database java file is attempting to connection to a mysql vm server.
    Currently trying to fix the connection issue.

--- app/src/main/java/com/groupwork/gymlads/Database.java
+++ app/src/main/java/com/groupwork/gymlads/Database.java
@@ -4,6 +4,12 @@ import android.content.Context;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.Driver;
+
+
 
 /**
  * Created by up826133 on 22/02/2018.
@@ -11,16 +17,30 @@ import java.sql.DriverManager;
 
 public class Database {
 
-    public static void main(String argws[]){
-        String url = "jdbc:mysql:http://up826133.myvm.port.ac.uk/";
-        String dbName = "up826133";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "root";
-        String password = "root";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url+dbName,userName,password);
+    Scanner myScanner = new Scanner(System.in);
+    public static void main(String args[]){
+    Database d = new Database();
+    d.run();
+    }
 
+    public void run(){
+
+        String url = "jdbc:mysql://35.195.212.24:3306/";
+        String dbName = "up826133";
+        String userName = "android";
+        try {
+            Class.forName("java.sql.Driver");
+            Connection conn = DriverManager.getConnection(url+dbName, userName, "Mysql123");
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM TEST");
+            while(rs.next()){
+                System.out.println(rs.getString("testId"));
+
+            }
+            rs.close();
+            st.close();
+            conn.close();
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
