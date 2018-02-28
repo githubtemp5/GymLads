0dfc8c2cc893019addf67a61a5d62777e1b69188
up826133 <up826133@myport.ac.uk>
2018-02-28 15:17:23 +0000

    Implementing MariaDB
    Added the correct jar file for mariadb, working on implementing it.

--- app/src/main/java/com/groupwork/gymlads/Database.java
+++ app/src/main/java/com/groupwork/gymlads/Database.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.provider.ContactsContract;
 
 
-import org.mariadb.jdbc.*;
+import org.mariadb.jdbc.Driver;
+
+import org.mariadb.jdbc.MariaDbConnection;
+import org.mariadb.jdbc.MariaDbStatement;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,17 +16,18 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 
+
 /**
  * Created by up826133 on 22/02/2018.
  */
 
 public class Database implements DatabaseInterface {
+    public static Scanner myScanner = new Scanner(System.in);
     private Connection conn;
     private String url;
     private String dbName;
     private String username;
     private String password;
-    public static Scanner myScanner = new Scanner(System.in);
 
     public Database(String _url, String _dbName, String _username, String _password){
         url = _url;
@@ -33,35 +37,39 @@ public class Database implements DatabaseInterface {
     }
 
 
+//    public void run(){
+//
+//
+//        try {
+//
+//            Statement st = conn.createStatement();
+//            //PreparedStatement p = new Pr
+//           // ResultSet rs = st.executeQuery("SELECT * FROM TEST");
+////            System.out.println("HERE");
+////            while(rs.next()){
+////                System.out.println(rs.getString("testId"));
+////
+////            }
+////            rs.close();
+//            st.close();
+//            conn.close();
+//            conn.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
     public String encrypt(String pass){
         return pass;
     }
-    public void run(){
 
 
-        try {
 
-            Statement st = conn.createStatement();
-            //PreparedStatement p = new Pr
-            ResultSet rs = st.executeQuery("SELECT * FROM TEST");
-            System.out.println("HERE");
-            while(rs.next()){
-                System.out.println(rs.getString("testId"));
-
-            }
-            rs.close();
-            st.close();
-            conn.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 
     public void establishConnection(){
         try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName("org.mariadb.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection(url+dbName, username, password);
             System.out.println("Connection established");
         }
@@ -69,6 +77,9 @@ public class Database implements DatabaseInterface {
             System.out.println("Error");
         }
     }
+
+
+
 
     public ResultSet selectQuery(String query){
         ResultSet rs = null;
@@ -99,25 +110,29 @@ public class Database implements DatabaseInterface {
     }
 
     public String deleteQuery(String query){
+
         return query;
     }
 
+
+
     public static void main(String args[]){
         System.out.println("Enter the password");
-        Database databaseInstance = new Database("jdbc:mysql://35.195.24.80:3306/", "up826133", "up826133", myScanner.nextLine());
+        Database databaseInstance = new Database("jdbc:mariadb://35.195.101.184:3306/", "up826133", "up826133", myScanner.nextLine());
         databaseInstance.establishConnection();
 
-        ResultSet rs = databaseInstance.selectQuery("SELECT * FROM Test");
+        //  ResultSet rs = databaseInstance.selectQuery("SELECT * FROM Test");
     }
 }
+
 
 
 interface DatabaseInterface{
 
     void establishConnection();
-    ResultSet selectQuery(String query);
-    ResultSet selectQuery(String query, String columnName);
-   // ResultSet selectQuery(ResultSet rs, String query, String columnName);
+    //  ResultSet selectQuery(String query);
+    // ResultSet selectQuery(String query, String columnName);
+    // ResultSet selectQuery(ResultSet rs, String query, String columnName);
     String insertQuery(String query);
     String updateQuery(String query);
     String deleteQuery(String query);
