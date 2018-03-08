0fdda03933444782218eab5a907ebd415ab10818
up826133 <up826133@myport.ac.uk>
2018-03-08 14:41:09 +0000

    Fixed error program at running state

--- app/src/main/java/com/groupwork/gymlads/MainMenu.java
+++ app/src/main/java/com/groupwork/gymlads/MainMenu.java
@@ -1,5 +1,6 @@
 package com.groupwork.gymlads;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -16,9 +18,19 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        ImageButton planButton = (ImageButton) findViewById(R.id.imageButton);
+        planButton.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        Intent a = new Intent(getApplicationContext(), ProfileActivity.class);
+                        a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(a);
+                    }
+                }
+        );
     }
 
 
 
-    }
+}
 
