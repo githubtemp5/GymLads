9b6acda23cb46a83f49d29df5efbabf6594c46ad
up780468 <36039887+up780468@users.noreply.github.com>
2018-03-14 16:03:48 +0000

    Register on signup success
    added functionality to open the login page when a user sucessfully registers

--- app/src/main/java/com/groupwork/gymlads/Register.java
+++ app/src/main/java/com/groupwork/gymlads/Register.java
@@ -1,5 +1,6 @@
 package com.groupwork.gymlads;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -50,7 +51,8 @@ public class Register extends AppCompatActivity {
     }
 
     public void onSignupSuccess() {
-
+        Intent intent = new Intent(this, Plan.class);
+        startActivity(intent);
     }
 
     public boolean validate() {
