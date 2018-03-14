a73a6bc955f08708d05a63ef7b25a1401d5bfb31
up780468 <36039887+up780468@users.noreply.github.com>
2018-03-14 17:09:27 +0000

    initial home screen functionality
    Added buttons to navigate to other activities

---
+++ app/src/main/java/com/groupwork/gymlads/HomeScreen.java
@@ -0,0 +1,49 @@
+package com.groupwork.gymlads;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreen extends AppCompatActivity {
+    Button registerPageBtn, loginPageBtn;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        registerPageBtn = (Button) findViewById(R.id.registerPageBtn);
+        registerPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRegBtnClick();
+            }
+        });
+
+        loginPageBtn = (Button) findViewById(R.id.loginPageBtn);
+        loginPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginBtnClick();
+            }
+        });
+    }
+
+    public void onRegBtnClick() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+    public void onLoginBtnClick() {
+        Intent intent = new Intent(this, Session.class);
+        startActivity(intent);
+    }
+
+
+}
