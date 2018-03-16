a6f9d7399875cd9dde3cb82da5f331605315357a
githubtemp5 <30862041+githubtemp5@users.noreply.github.com>
2018-03-16 20:04:02 +0000

    Personalised plan

--- app/src/main/java/com/groupwork/gymlads/SessionGUI.java
+++ app/src/main/java/com/groupwork/gymlads/SessionGUI.java
@@ -1,5 +1,7 @@
 package com.groupwork.gymlads;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,21 +10,40 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 
 public class SessionGUI extends AppCompatActivity {
 
+    public static String currentlySelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_gui);
-//days of the week inputted as string.This is linked with the personalised plan
+
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         ListAdapter planAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, days);
         ListView PlanListView = (ListView) findViewById(R.id.PlanListView);
         PlanListView.setAdapter(planAdapter);
 
-    }
+        PlanListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
 
+                if(position==0){
+                    currentlySelected = "mon";
+                    Intent i = new Intent(getApplicationContext(), PersonalisedPlan.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+                else if(position==1){
+                    currentlySelected = "tue";
+                    Intent i = new Intent(getApplicationContext(), PersonalisedPlan.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            }
+    });
+
+    }
 }
