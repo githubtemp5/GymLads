4fe6e736054c5699199cab1f50796120769fb7c6
Jibril Maikifi <jibrilmaikifi@gmail.com>
2018-03-16 08:20:25 +0000

    Update and rename Weight_Tracker.java to weight_tracker.java

--- app/src/main/java/com/groupwork/gymlads/weight_tracker.java
+++ app/src/main/java/com/groupwork/gymlads/weight_tracker.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Weight_Tracker extends AppCompatActivity {
+public class weight_tracker extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
