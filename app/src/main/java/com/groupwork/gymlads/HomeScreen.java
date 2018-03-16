311a1a78a5677697232298c1313f18d53b302a1f
danthekings <32768470+danthekings@users.noreply.github.com>
2018-03-16 19:35:32 +0000

    updated comments

--- app/src/main/java/com/groupwork/gymlads/HomeScreen.java
+++ app/src/main/java/com/groupwork/gymlads/HomeScreen.java
@@ -1,8 +1,8 @@
 package com.groupwork.gymlads;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
+import android.app.Activity;//importing the application page 
+import android.content.Intent;//importing intent will allow us to switch pages if nessessary 
+import android.os.Bundle;//all of the opreating system 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,29 +11,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
-    Button registerPageBtn, loginPageBtn, skipBtn;
+public class HomeScreen extends AppCompatActivity {// this is the java class which add the functianlity to the start page of the program 
+    Button registerPageBtn, loginPageBtn, skipBtn;//which has three once click or tab button 
     @Override
 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {// this function allow the us to set the home screen when opening the application 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        registerPageBtn = (Button) findViewById(R.id.registerPageBtn);
+        registerPageBtn = (Button) findViewById(R.id.registerPageBtn);// contstantly checking if the button are click
         registerPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onRegBtnClick();
+                onRegBtnClick(); // if it is click on the register page it will take the user to register page 
             }
         });
 
-        loginPageBtn = (Button) findViewById(R.id.loginPageBtn);
+        loginPageBtn = (Button) findViewById(R.id.loginPageBtn); // login page where it will take the user to login page 
         loginPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onLoginBtnClick();
             }
         });
-        skipBtn = (Button) findViewById(R.id.skipBtn);
+        skipBtn = (Button) findViewById(R.id.skipBtn);// skip page allow the user to skip the hassle of entering their detail to use the application 
         skipBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,7 +41,7 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
     }
-
+//if the function above get a click and find which is click then the code below will be ran to take to next page 
     public void onRegBtnClick() {
         Intent intent = new Intent(this, Register.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
