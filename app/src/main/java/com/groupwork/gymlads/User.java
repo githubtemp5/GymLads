a4d1957edeb7c885dd1ebf19f2a8ceef05d900fb
Alen Sunny <35536085+alensunny19@users.noreply.github.com>
2018-03-16 20:11:17 +0000

    Update User.java
    just basic comments on what each function does

--- app/src/main/java/com/groupwork/gymlads/User.java
+++ app/src/main/java/com/groupwork/gymlads/User.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Created by up826133 on 21/02/2018.
  */
 
-
+//register page with values displayed on teh page
 public class                                                                                                                                                                                                                                                                                                           User {
     private String userName;
     private String fullName;
@@ -18,7 +18,7 @@ public class                                                                    
     private int weight;
     private int height;
     private ArrayList<Plan> userPlans;
-
+// The strings and int that are inputted into the application
     public User(String _userName, String _fullName, String _email, int _age , String _gender, int _weight, int _height){
 
         userName = _userName;
@@ -31,7 +31,7 @@ public class                                                                    
         userPlans = new ArrayList<Plan>();
 
         }
-
+//returns the username ect to see if valid
     public String getUserName() {
         return userName;
     }
