27949a185c50f54e03510b02d898371931606d2e
jphawkes <35536021+jphawkes@users.noreply.github.com>
2018-03-16 20:15:11 +0000

    Comments for register page

--- app/src/main/java/com/groupwork/gymlads/Register.java
+++ app/src/main/java/com/groupwork/gymlads/Register.java
@@ -17,7 +17,9 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-
+/**
+ * A register screen that allows the user to create a account
+ */
 public class Register extends AppCompatActivity {
     private EditText et_name, et_email, et_username, et_password, et_cpassword;
     private String name, email, username, password, cpassword;
@@ -37,6 +39,7 @@ public class Register extends AppCompatActivity {
         et_password = (EditText) findViewById(R.id.password);
         et_cpassword = (EditText) findViewById(R.id.cpassword);
         termsCheckBox = (CheckBox) findViewById(R.id.termsCheckBox);
+//runs the cancel function when button is clicked
         cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -44,7 +47,7 @@ public class Register extends AppCompatActivity {
                 cancel();
         }
         });
-
+//runs the register function when button is clicked
         regButton = (Button) findViewById(R.id.regButton);
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +57,9 @@ public class Register extends AppCompatActivity {
 
     });}
 
+    /**
+ * This function displays a error message if the user inputs fail to validate. If it validates it runs the signup sucess function.
+ */
     public void register() {
         initialise();
         if (!validate()) {
@@ -63,11 +69,16 @@ public class Register extends AppCompatActivity {
         }
     }
 
+    /**
+ * A function which changes the activity to the home screen when the button is pressed
+ */
     public void cancel(){
         Intent intent = new Intent(this, HomeScreen.class);
         startActivity(intent);
     }
-
+/**
+ * Checks the username doesn't already exist. If both the username and password are valid they are added to storage arrays.
+ */
     public void onSignupSuccess() {
         if(usernames.contains(Register.this.et_username.getText().toString())){
             et_username.setError("This username already exists");
@@ -77,33 +88,41 @@ public class Register extends AppCompatActivity {
             usernames.add(Register.this.et_username.getText().toString());
             System.out.println(usernames.get(0));
             passwordArray();
+            //clears fields once username and password are stored in the array.
             et_username.setText(null);
             et_password.setText(null);
             et_name.setText(null);
             et_email.setText(null);
             et_cpassword.setText(null);
             termsCheckBox.setChecked(false);
+            //swaps the activity to the main menu
             Intent intent = new Intent(this, MainMenu.class);
             startActivity(intent);
         }
 
     }
-
+/**
+ * Adds valid password to array
+ */
     public void passwordArray(){
         passwords.add(Register.this.et_password.getText().toString());
         System.out.println(passwords.get(0));
     }
-
+/**
+ * Checks fields are not left empty and they dont exceed the maximum specified length
+ */
     public boolean validate() {
         boolean valid = true;
         if (name.isEmpty() || name.length() > 32) {
             et_name.setError("Please enter a valid name");
             valid = false;
         }
+        //checks email address is formatted correctly
         if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             et_email.setError("Please enter a valid email address");
             valid = false;
         }
+        //checks username does not exceed maximumm length
         if (username.isEmpty() || username.length() > 20) {
             et_username.setError("Please enter a valid username");
             valid = false;
@@ -113,10 +132,12 @@ public class Register extends AppCompatActivity {
             et_password.setError("Please enter a valid password");
             valid = false;
         }
+        //checks cpassword is the same as the original password entered
         if (cpassword.isEmpty() || cpassword.length() > 20 || !cpassword.equals(password)) {
             et_cpassword.setError("Please enter a valid Password");
             valid = false;
         }
+        //checks terms and conditions box is checked and displays error mesage if not
         if (!termsCheckBox.isChecked()) {
             termsCheckBox.setError("Please accept the terms and conditions");
             valid = false;
@@ -124,7 +145,7 @@ public class Register extends AppCompatActivity {
         return valid;
     }
 
-
+//puts inputs into a useable format
     public void initialise(){
         name = et_name.getText().toString().trim();
         email = et_email.getText().toString().trim();
